74a9729fbddcea3535f7e542bdc34adeadda267f
Carina Veil <159014789+cayobro@users.noreply.github.com>
2026-09-03 20:07:07 +0200

    Refactor data loading and plotting for new dataset
    Updated the data loading process to use 'dataset_without_modules.pickle' and modified the plotting code to handle new data format.

--- load_data.py
+++ load_data.py
@@ -9,15 +9,28 @@ The impedance data is stored patient-wise in ReducedMeasurement objects which co
 """
 
 
-with open('dataset.pickle', 'rb') as h:
+with open('dataset_without_modules.pickle', 'rb') as h:
     data = pickle.load(h)
 
-# Example: Plot healthy measurements of patient 1
+# # Example: Plot healthy measurements of patient 1
+# p1_h = data['patient1']['healthy']
+# fig, ax = plt.subplots(2, 1, sharex=True)
+# ax[0].set_xscale('log')
+# ax[0].set_ylabel('$|Z|$')
+# ax[1].set_ylabel('$\\phi(Z)$')
+# for measurement in p1_h:
+#     ax[0].plot(measurement.frequency, np.abs(measurement.z))
+#     ax[1].plot(measurement.frequency, np.angle(measurement.z))
+
+# New data without object
 p1_h = data['patient1']['healthy']
 fig, ax = plt.subplots(2, 1, sharex=True)
 ax[0].set_xscale('log')
 ax[0].set_ylabel('$|Z|$')
 ax[1].set_ylabel('$\\phi(Z)$')
 for measurement in p1_h:
-    ax[0].plot(measurement.frequency, np.abs(measurement.z))
-    ax[1].plot(measurement.frequency, np.angle(measurement.z))
+    frequency = np.array(measurement['frequency'])  # Convert back to numpy array
+    z = np.array(measurement['z'])  # Convert back to numpy array
+    ax[0].plot(frequency, np.abs(z))
+    ax[1].plot(frequency, np.angle(z))
+plt.show()
